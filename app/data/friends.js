9bdeafe78cb7a55f3b2189398e059a3c4f25ba75
Firecraker <mezamic000@gmail.com>
2020-07-29 13:44:51 -0500

    created my array of friends

---
+++ app/data/friends.js
@@ -0,0 +1,102 @@
+var friends = [
+  {
+    name: "Michelle Obama",
+    photo: "https://media2.s-nbcnews.com/j/newscms/2017_02/1187283/michelle-obama-style-today-161216-21_36c0986d5429b037714c34699c28c4a1.fit-760w.jpg",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Jennifer Lopez",
+    photo: "https://www.hellomagazine.com/imagenes/celebrities/2019112681114/jennifer-lopez-reveals-sadness-over-twins-emme-max/0-391-235/jennifer-lopez-sadness-over-twins-t.jpg",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Chrissy Teigen",
+    photo: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/chrissy-teigen-1552328438.jpg?crop=0.835xw:1.00xh;0.104xw,0&resize=980:*",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Beyonce",
+    photo: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gettyimages-920233020-1552329031.jpg?crop=1xw:0.9984189723320158xh;center,top&resize=980:*",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Reese Witherspoon",
+    photo: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/18512907-654375948085554-6499271235317268480-n-1584732953.jpg?crop=0.907xw:0.597xh;0.00481xw,0.165xh&resize=1200:*",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "Serena Williams",
+    photo: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/serena-and-olympia-1552330265.jpg?crop=0.835xw:1.00xh;0.119xw,0&resize=980:*",
+    scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  }
+
+];
+
+// Here we export the array. This makes it accessible to other files using require.
+module.exports = friends;
